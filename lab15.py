eb9cba5647952ac6b64aeef669f5a8fb799ab501
Serafiend7 <eperegoy@csumb.edu>
2025-03-25 16:35:53 -0700

    Added Code
    Added lab15 code to github repository

---
+++ lab15.py
@@ -0,0 +1,14 @@
+from flask import (Flask,render_template)
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+
+@app.route('/')
+def hello():
+  return '<h1>Flask App</h1><p>afaik</p><p>idk</p>'
+
+@app.route('/ethan')
+def template():
+  return render_template('template.html')
